434ebaa3a67c835acb731f99e7465c88c0a89459
NickLatkovich <nicklatkovich@gmail.com>
2017-11-18 08:40:25 +0300

    Saving processing

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 
-var fetch = require('node-fetch')
+const fetch = require('node-fetch')
+const fs = require('fs')
 
 let heroes
 
 const SKILLS = [, "normal", "high", "very high"] // помойки
 const API = 'https://api.opendota.com/api/'
+const MAX_TWR_DMG_QUOTIENT = 29949.0 / 12250
 
 var getFetch = async (url) => {
     let response = await fetch(url)
@@ -26,35 +28,63 @@ async function getMatch(id) {
     return result
 }
 
-function getHeroInfo(player) {
+function getPlayerCommonInfo(player) {
     return heroes[player.hero_id].localized_name +
         (player.account_id == null ? '' : ' (id: ' + player.account_id + ')')
 }
 
 async function main( ) {
+    // for restart insert "start matchId + 1" (for example 3567671556) into this and run:
+    fs.writeFileSync('data/lastMatch.txt', 3567671556 + 1)
+
+    let lastMatchId = fs.readFileSync('data/lastMatch.txt')
+
     heroes = await getHeroes( )
 
-    let match = await getMatch(3567671555)
-    console.log( )
-    console.log('radiant pick:')
-    for (let i = 0; i < 5; i++) {
-        console.log(getHeroInfo(match.players[i]))
+    try {
+        for (let i = 0; i < 100; i++) {
+            let matchId = lastMatchId - 1
+
+            function skip(cause) {
+                console.log(matchId + ' was skipped' + (cause != undefined ? ' due to ' + cause : ''))
+                i--
+                lastMatchId--
+            }
+
+            let match = await getMatch(matchId)
+            let players = match.players
+            if (players == undefined || players.length != 10) {
+                skip('wrong number of players')
+                continue
+            }
+            let data = ""
+            let twr_dmg = [0, 0]
+            players.forEach(function(element, index) {
+                data += element.hero_id + ':0.5\n'
+                twr_dmg[index < 5 ? 0 : 1] += element.tower_damage
+            }, this)
+            let radiant_win = match.radiant_win
+            data += (radiant_win ? 1 : 0) + '\n'
+            let win_side = radiant_win ? 0 : 1
+            let lose_side = 1 - win_side
+            if (twr_dmg[win_side] < 12250) {
+                skip('uncertainty')
+                continue
+            }
+            let quotient = 1 - twr_dmg[lose_side] / (2 * MAX_TWR_DMG_QUOTIENT * twr_dmg[win_side])
+            if (quotient <= 0.5 || quotient > 1.0) {
+                throw "WTF???"
+            }
+            data += quotient + '\n'
+            fs.writeFileSync('data/matches/' + matchId + '.txt', data)
+            console.log(matchId + ' is processed')
+            lastMatchId--
+        }
+        fs.writeFileSync('data/lastMatch.txt', lastMatchId)
+    } catch (e) {
+        console.error(e)
+        fs.writeFileSync('data/lastMatch.txt', lastMatchId)
     }
-
-    console.log( )
-    console.log('dire pick:')
-    for (let i = 5; i < 10; i++) {
-        console.log(heroes[match.players[i].hero_id].localized_name)
-    }
-
-    console.log( )
-    console.log('win: ' + (match.radiant_win ? 'radiant' : 'dire'))
-
-    console.log( )
-    console.log('skill: ' + SKILLS[match.skill])
-
-    // console.log( )
-    // console.log('time: ' + match.duration)
 }
 
 main( )
